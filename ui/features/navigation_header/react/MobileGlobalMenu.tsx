4fcbc3125b6e468fb040ce4b214f333cb11c6e1e
steven.ntucool <stevenyang1222@ntu.edu.tw>
2025-07-31 09:01:04 +0800

    fix: Hide history icon #186

--- ui/features/navigation_header/react/MobileGlobalMenu.tsx
+++ ui/features/navigation_header/react/MobileGlobalMenu.tsx
@@ -293,7 +293,8 @@ export default function MobileGlobalMenu(props: Props) {
           </List.Item>
         ))}
 
-        <List.Item>
+        {/* Hide History Icon #186 */}
+        {/* <List.Item>
           <ToggleDetails
             iconPosition="end"
             fluidWidth={true}
@@ -312,7 +313,7 @@ export default function MobileGlobalMenu(props: Props) {
               <HistoryList />
             </View>
           </ToggleDetails>
-        </List.Item>
+        </List.Item> */}
 
         {/* Hide Help link #116 */}
         {/* <List.Item>
