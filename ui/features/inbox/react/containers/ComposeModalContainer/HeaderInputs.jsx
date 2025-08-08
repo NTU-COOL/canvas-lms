88bea4bf22b83f26200fb18b6fcae9eb2f8d4c04
steven.ntucool <stevenyang1222@ntu.edu.tw>
2025-08-08 10:55:12 +0800

    feat: Review Calender Translation #544

--- ui/features/inbox/react/containers/ComposeModalContainer/HeaderInputs.jsx
+++ ui/features/inbox/react/containers/ComposeModalContainer/HeaderInputs.jsx
@@ -118,7 +118,8 @@ const HeaderInputs = props => {
             title={
               <PresentationContent>
                 <Text id="address-book-form" size="small">
-                  {I18n.t('To')}
+                  {/* Cool Mailbox Customize #537 */}
+                  {ENV.LOCALE === 'zh-Hant' ? '收件者' : I18n.t('To') }
                 </Text>
               </PresentationContent>
             }
@@ -137,7 +138,8 @@ const HeaderInputs = props => {
                 addressBookMessages={props.addressBookMessages}
                 courseContextCode={props.selectedContext?.contextID || ''}
                 placeholder={I18n.t('Insert or Select Names')}
-                addressBookLabel={I18n.t('To')}
+                // Cool Mailbox Customize #537
+                addressBookLabel={ENV.LOCALE === 'zh-Hant' ? '收件者' : I18n.t('To')}
               />
             }
             shouldGrow={true}
